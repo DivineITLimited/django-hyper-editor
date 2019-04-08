be7d284893c13f345c5973392ec8bfc2b57a180e
Shimul Chowdhury <shimul@divine-it.net>
2019-04-08 22:55:39 +0600

    Added license and fixed setup.py

--- setup.py
+++ setup.py
@@ -1,32 +1,36 @@
-import os
+import pathlib
 from setuptools import find_packages, setup
 
-with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
-    README = readme.read()
+HERE = pathlib.Path(__file__).parent
 
-# allow setup.py to be run from any path
-os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+README = (HERE / "README.md").read_text()
 
 setup(
     name='hypereditor',
-    version='0.1.0',
+    version='0.1.0-rc1',
     packages=find_packages(),
     include_package_data=True,
-    license='Divine IT Limited 2018',
+    license='MIT',
     description='Hyper Editor Integration With Django',
     long_description=README,
     author='Shimul Chowdhury',
     author_email='shimul@divine-it.net',
-    install_requires = [
-    ],
-    dependency_links = [
+    install_requires=[
+        "Django>=1.11,<2.3",
     ],
     classifiers=[
         'Environment :: Web Environment',
-        'Framework :: Django',
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.6'
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Framework :: Django',
+        'Framework :: Django :: 2.0',
+        'Framework :: Django :: 2.1',
+        'Framework :: Django :: 2.2',
     ],
 )
