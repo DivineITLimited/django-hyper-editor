2ff9f9d8ec18a67c9b63802d756c798000324f33
Shimul Chowdhury <shimul@divine-it.net>
2019-04-15 12:04:09 +0600

    Tests for template tags

---
+++ hypereditor/tests/mocks.py
@@ -0,0 +1,24 @@
+TEXT_BLOCK = {
+    "type": "text",
+    "children": [],
+    "settings": {
+        "text": "Sample Text"
+    },
+    "id": "i1554530213_4",
+    "general": {
+        "padding": {
+            "unit": "px",
+            "left": None,
+            "top": None,
+            "right": None,
+            "bottom": None
+        },
+        "margin": {
+            "unit": "px",
+            "left": None,
+            "top": None,
+            "right": None,
+            "bottom": None
+        }
+    }
+}
---
+++ hypereditor/tests/test_templatetags.py
@@ -0,0 +1,30 @@
+import json
+from django.test import SimpleTestCase
+from django.template import Context, Template
+from hypereditor.tests import mocks
+
+
+class HyperRenderTemplateTagTest(SimpleTestCase):
+    """Tests for hyper_render template tag"""
+
+    def test_render(self):
+        context = Context({'content': json.dumps([mocks.TEXT_BLOCK])})
+        template_to_render = Template(
+            '{% load hyper_tags %}'
+            '{% hyper_render content %}'
+        )
+        rendered_template = template_to_render.render(context)
+        self.assertInHTML('<div id="i1554530213_4">\nSample Text\n</div>', rendered_template)
+
+
+class HyperPreviewTemplateTagTest(SimpleTestCase):
+    """Tests for hyper_preview template tag"""
+
+    def test_preview(self):
+        context = Context({'content': mocks.TEXT_BLOCK})
+        template_to_render = Template(
+            '{% load hyper_tags %}'
+            '{% hyper_preview content %}'
+        )
+        rendered_template = template_to_render.render(context)
+        self.assertInHTML('<div id="i1554530213_4">\nSample Text\n</div>', rendered_template)
